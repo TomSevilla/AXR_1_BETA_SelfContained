74cd1e0e132e027669a6f4089cbf48f81b249cbf
Thomas Sevilla <tomassevilla2@gmail.com>
2016-08-22 11:59:54 -0400

    Initial Commit

---
+++ AXR_1_BETA_SelfContained/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AXR_1_BETA_SelfContained
+//
+//  Created by Thomas Sevilla on 8/22/16.
+//  Copyright © 2016 Thomas Sevilla. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
